16b55b9e9cb8241996114ce4f1e081a4a1e03f38
Hueyzaa <giahuy14sp@gmail.com>
2026-03-15 18:41:45 +0700

    fix: env

--- backend/dist/configs/env.config.js
+++ backend/dist/configs/env.config.js
@@ -7,18 +7,20 @@ exports.envConfig = (0, config_1.registerAs)('env', () => ({
     port: Number(process.env.PORT) || 9999,
     database: {
         type: 'mysql',
-        host: process.env.DB_HOST || 'localhost',
-        port: Number(process.env.DB_PORT) || 3306,
-        username: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '123456',
-        database: process.env.DB_NAME || 'web_portfolio',
+        host: process.env.DB_HOST || process.env.CORE_DATABASE_HOST || 'localhost',
+        port: Number(process.env.DB_PORT || process.env.CORE_DATABASE_PORT) || 3306,
+        username: process.env.DB_USER || process.env.CORE_DATABASE_USER || 'root',
+        password: process.env.DB_PASSWORD || process.env.CORE_DATABASE_PASS || '123456',
+        database: process.env.DB_NAME || process.env.CORE_DATABASE_NAME || 'web_portfolio',
         autoLoadEntities: true,
         synchronize: process.env.NODE_ENV === 'development',
     },
-    system_name: process.env.VITE_WEB_NAME || 'Web Portfolio',
-    cors_origin: process.env.CORS_ORIGIN || '*',
+    system_name: process.env.VITE_WEB_NAME ||
+        process.env.CORE_SYSTEM_NAME ||
+        'Web Portfolio',
+    cors_origin: process.env.CORS_ORIGIN || process.env.CORE_SYSTEM_FRONTEND_URL || '*',
     jwt_secret_key: process.env.CORE_JWT_SECRET_KEY || 'web_portfolio_secret_key_2026',
     jwt_expires_time: process.env.CORE_EXPIRES_TIME || '1d',
-    upload_path: process.env.UPLOAD_PATH || 'uploads',
+    upload_path: process.env.UPLOAD_PATH || process.env.CORE_UPLOAD_DIR || 'uploads',
 }));
 //# sourceMappingURL=env.config.js.map
